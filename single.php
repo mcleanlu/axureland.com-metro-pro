55510157af20fc946f5a096fa41c6fb83f0fe0f0
d1ofmany <nikola.barkovic@gmail.com>
2014-03-11 12:19:14 +0100

    Demo & Download edit
    Demo & Download edit for easyer styling

--- single.php
+++ single.php
@@ -2,14 +2,19 @@
 
 function add_acf()
 {
+
+	$data = '<div class="demo-download">';
+	
 	if(get_field('demo'))
 	{
-		echo '<a href="' . get_field('demo') . '" target="_blank"><button>Demo</button></a> ';
+		$data .= '<a href="' . get_field('demo') . '" target="_blank"><button>Demo</button></a> ';
 	}
 	if(get_field('download'))
 	{
-		echo '<a href="' . get_field('download') . '" target="_blank"><button>Download</button></a>';
+		$data .= '<a href="' . get_field('download') . '" target="_blank"><button>Download</button></a>';
 	}
+	
+	echo '</div>' . $data;
 }
 
 add_action( 'genesis_entry_content', 'add_acf' );
